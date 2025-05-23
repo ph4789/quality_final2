15057c9ebb5fe6ee7957ce1271e173f605d3061b
Pedro Gomes <pedrohenrykue.ph7@gmail.com>
2025-05-22 21:54:43 -0300

    Teste simulação de usuário teste_03.py

--- teste/teste_03.py
+++ teste/teste_03.py
@@ -1,1 +1,12 @@
+# Teste 03 – Simulação de criação de usuário com POST
+import requests
 
+def test_criar_usuario_reqres():
+    url = "https://reqres.in/api/users"
+    payload = {
+        "name": "Pedro",
+        "job": "QA Tester"
+    }
+    response = requests.post(url, json=payload)
+    assert response.status_code == 201
+    assert response.json()["name"] == "Pedro"
