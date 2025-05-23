aee8f22e3a7947c7e71ad2c76e99bc0308c12ad0
Pedro Gomes <pedrohenrykue.ph7@gmail.com>
2025-05-22 21:53:50 -0300

    Teste de verificação de Api teste_01.py

--- teste/teste_01.py
+++ teste/teste_01.py
@@ -1,1 +1,7 @@
+# Teste 01 – Verificação de status da API ReqRes
+import requests
 
+def test_status_api_reqres():
+    url = "https://reqres.in/api/users/2"
+    response = requests.get(url)
+    assert response.status_code == 200
