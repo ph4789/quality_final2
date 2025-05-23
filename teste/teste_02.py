547b803ec28fa42d681ad6190fb75ed178ee0906
Pedro Gomes <pedrohenrykue.ph7@gmail.com>
2025-05-22 21:52:16 -0300

    Adiciona código do teste_02.py

--- teste/teste_02.py
+++ teste/teste_02.py
@@ -1,1 +1,10 @@
+# Teste 02 – Verificação de dados retornados pela API ReqRes
+import requests
+
+def test_dados_usuario_reqres():
+    url = "https://reqres.in/api/users/2"
+    response = requests.get(url)
+    data = response.json()
+    assert data["data"]["id"] == 2
+    assert "email" in data["data"]
 
